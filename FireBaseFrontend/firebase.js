7a2d82312b6ef3d03f7daeb89f7ba55068d09ba6
Shmarte.1970 <shmarte@gmail.com>
2021-12-14 17:01:10 +0100

    14122021/17:00

---
+++ FireBaseFrontend/firebase.js
@@ -0,0 +1,18 @@
+ type="module"
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBpXYfGlNI46053OANVc3WXSfQZED-Gnkc",
+    authDomain: "crudjs1970.firebaseapp.com",
+    projectId: "crudjs1970",
+    storageBucket: "crudjs1970.appspot.com",
+    messagingSenderId: "589934599422",
+    appId: "1:589934599422:web:65d55037ff98679f9299af"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
